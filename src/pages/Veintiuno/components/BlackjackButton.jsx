65b314088fdd953d92fdf64619903c4954be72b9
AlanHiguera <alanhiguera144@gmail.com>
2025-09-23 21:47:34 -0300

    add: tailwind

--- src/pages/Veintiuno/components/BlackjackButton.jsx
+++ src/pages/Veintiuno/components/BlackjackButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Blackjack.css";
+import "../Blackjack.css";
 
 const BlackjackButton = ({ disabled, color = "blue", onClick, children }) => {
   const colorClass = `blackjack-button blackjack-button-${color}`;
